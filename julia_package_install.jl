542a388d597807479a6ba2c1cbef6c9c530ef4ef
release200428 <release200428@sawato.jp>
2020-05-01 16:22:18 +0900

    julia installer added.

---
+++ julia_package_install.jl
@@ -0,0 +1,32 @@
+print("gail-driver installer")
+if VERSION != v"0.6.4"
+  print("version is wrong. it must be 0.6.4")
+  return
+end
+
+gail_root = ENV["GAIL_DRIVER_GITHUB"]
+exit()
+usingPkg.clone(gail_root * "/Vec.jl")
+Pkg.build("Vec.jl")
+Pkg.clone("https://github.com/JuliaIO/VideoIO.jl")
+Pkg.clone("https://github.com/JuliaGraphics/Cairo.jl")
+Pkg.checkout("VideoIO.jl", "mindeps")
+Pkg.checkout("Cairo.jl", "app")
+Pkg.build("VideoIO.jl")
+Pkg.build("Cairo.jl")
+Pkg.clone(gail_root * "/AutomotiveDrivingModels.jl")
+Pkg.build("AutomotiveDrivingModels.jl")
+Pkg.clone(gail_root * "/AutoViz.jl")
+Pkg.build("AutoViz.jl")
+Pkg.clone(gail_root * "/ForwardNets.jl")
+Pkg.build("ForwardNets.jl")
+Pkg.clone(gail_root * "/NGSIM.jl")
+Pkg.build("NGSIM.jl")
+Pkg.clone(gail_root * "/Records.jl")
+Pkg.build("Records.jl")
+using Vec
+using AutomotiveDrivingModels
+using NGSIM
+using AutoViz
+using Records
+using ForwardNets
