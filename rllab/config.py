63789fc789e1a07b516fd5d5b13d99735ea5a1bd
corydoras69dev <corydoras69dev@sawato.jp>
2020-05-05 16:02:42 +0900

    path changed.

--- rllab/config.py
+++ rllab/config.py
@@ -69,7 +69,7 @@ MUJOCO_KEY_PATH = osp.expanduser("~/.mujoco")
 ENV = {}
 
 auto2D_path = "~/gail/gail-driver/julia/envs"
-expert_traj_path = "/home/shusakusawato/.julia/v0.6/NGSIM.jl/data"
+expert_trajs_path = "/home/shusakusawato/.julia/v0.6/NGSIM.jl/data"
 model_path = "/home/shusakusawato/.julia/v0.6/AutomotiveDrivingModels.jl"
 
 if osp.exists(osp.join(osp.dirname(__file__), "config_personal.py")):
