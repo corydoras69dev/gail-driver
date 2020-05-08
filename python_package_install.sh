5a2a3fe10f025686a931bfd0868feee18e8f19e8
corydoras69dev <corydoras69dev@sawato.jp>
2020-05-08 16:59:32 +0900

    update.

---
+++ python_package_install.sh
@@ -0,0 +1,3 @@
+python -m pip install gym==0.7.3 joblib==0.12 yes cached_property
+python -m pip install theano==0.9.0  pyprind
+python -m pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0-cp27-none-linux_x86_64.whl
