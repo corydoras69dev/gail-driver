a702c594a616aa64b6776f3345e7d31c69722152
corydoras69dev <corydoras69dev@sawato.jp>
2020-05-16 15:43:52 +0900

    update.

--- seedmng/mng.py
+++ seedmng/mng.py
@@ -13,7 +13,7 @@ class SeedMng(Singleton):
 
     def __init__(self):
 
-    def set(self,input)
+    def set(self,input):
         self._root_seed = input
         self._system_seed = 0
         self._tf_graph_seed = 0
